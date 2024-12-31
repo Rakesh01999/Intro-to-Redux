a7fcc65675ec7e8677db6a8f0a18531637ac5541
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-31 22:00:41 +0600

    reduxjs/toolkit react-redux installed

--- redux-bootstrap/src/App.tsx
+++ redux-bootstrap/src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>Counter with Redux</h1>
+      <button>Increment</button>
       <div>0</div>
+      <button>Decrement</button>
     </div>
   );
 }
